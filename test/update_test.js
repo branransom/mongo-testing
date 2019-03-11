c6d2f744e6d1682ef283faa6b1938c87a35d25a8
Brandon Ransom <brandonransom@Brandons-iMac.local>
2019-03-10 21:28:15 -0400

    testing basic user schema

---
+++ test/update_test.js
@@ -0,0 +1,51 @@
+const assert = require("assert");
+const User = require("../src/User");
+
+describe("updating records", () => {
+  let joe;
+
+  beforeEach(done => {
+    joe = new User({ name: "Joe", postCount: 0 });
+    joe.save().then(() => done());
+  });
+
+  function assertName(operation, done) {
+    operation
+      .then(() => User.find({}))
+      .then(users => {
+        assert(users.length === 1);
+        assert(users[0].name === "Alex");
+        done();
+      });
+  }
+
+  it("instance type using set n save", done => {
+    joe.set("name", "Alex");
+    assertName(joe.save(), done);
+  });
+
+  it("a model instance can update", done => {
+    assertName(joe.update({ name: "Alex" }), done);
+  });
+
+  it("a model class can update", done => {
+    assertName(User.update({ name: "Joe" }, { name: "Alex" }), done);
+  });
+
+  it("a model class can update one record", done => {
+    assertName(User.findOneAndUpdate({ name: "Joe" }, { name: "Alex" }), done);
+  });
+
+  it("a model class can find a record with an Id and update", done => {
+    assertName(User.findByIdAndUpdate(joe._id, { name: "Alex" }), done);
+  });
+
+  xit("a user can have their postcount incremented by 1", done => {
+    User.update({ name: "Joe" }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then(user => {
+        assert(user.postCount === 1);
+        done();
+      });
+  });
+});
